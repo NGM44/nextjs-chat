bcf45e08d87b94b114eea64f8e2cfa829e02524e
NGM <nikhil@rulezero.com>
2024-07-01 23:43:18 +0530

    theme

--- app/overview/page.tsx
+++ app/overview/page.tsx
@@ -1,21 +1,12 @@
 'use client'
 import React from 'react'
-import FeatureCard from './card'
-import Headline from './Headline'
-import Testimonials from './Testimonal'
 import Feature from './Features'
 import FAQ from './Faq'
 import Footer from './Footer'
 import HeroPage from './Hero'
-import { useTheme } from 'next-themes'
-import { useEffect } from 'react'
 
 const page = () => {
-  const { setTheme, theme } = useTheme()
-  useEffect(() => {
-    console.log('LOG')
-    setTheme('dark')
-  }, [])
+
 
   return (
     <div>
--- components/providers.tsx
+++ components/providers.tsx
@@ -5,10 +5,13 @@ import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { ThemeProviderProps } from 'next-themes/dist/types'
 import { SidebarProvider } from '@/lib/hooks/use-sidebar'
 import { TooltipProvider } from '@/components/ui/tooltip'
+import { useState } from 'react'
 
 export function Providers({ children, ...props }: ThemeProviderProps) {
+  const [theme, setTheme] = useState('light');
+
   return (
-    <NextThemesProvider {...props}>
+    <NextThemesProvider {...props} themes={[theme]}>
       <SidebarProvider>
         <TooltipProvider>{children}</TooltipProvider>
       </SidebarProvider>
