85836fde940551d9fe3f751258e16ae9a85ae1df
NGM <nikhil@rulezero.com>
2024-08-01 12:01:20 +0530

    footer update

--- app/overview/Footer.tsx
+++ app/overview/Footer.tsx
@@ -207,7 +207,14 @@ export default function Footer() {
           </p>
         </div>
         <div className="mt-24">
-          <p className="text-[13px] text-gray-300">Risk Disclaimer</p>
+          <p className='text-[13px] text-gray-300'>Risk Disclaimer</p>
+<p className='text-[11px] text-gray-500'>
+The information provided on this website is for general informational purposes only and is not intended to be investment, legal, tax, or financial advice. All content is provided &apos;as is&apos; without any warranties, express or implied. While we strive to ensure that the information on this website is accurate and up-to-date, we make no representations or warranties of any kind about the completeness, accuracy, reliability, suitability, or availability with respect to the website or the information, products, services, or related graphics contained on the website.The content of this website is directed only at persons residing in India and is not intended for distribution to, or use by, any person in any jurisdiction where such distribution or use would be contrary to local law or regulation.Our advisory services are based on personal opinions and do not constitute an offer to buy or sell any securities or other financial instruments. Users are encouraged to seek advice from an independent financial advisor before making any investment decisions.We do not guarantee any specific outcomes or results from our investment advice or strategies. All decisions made by users are their sole responsibility.We may have interests or positions in the securities mentioned on this website, which could be considered a conflict of interest. These interests will be disclosed where appropriate.
+User Responsibility. Investing in financial markets involves risk, including the potential loss of principal. Past performance is not indicative of future results. It is important to do your own analysis before making any investment based on your own personal circumstances. The value of investments can go down as well as up due to market volatility and other factors. You may not get back the amount you originally invested.Users should independently verify all information before making any investment decisions. Reliance on any information provided on this website is solely at your own risk. You are solely responsible for your own investment decisions. We do not accept any liability for any loss or damage resulting from your reliance on the information provided on this website. 
+Contact Information. If you have any questions or concerns about our disclaimers, please contact us at <span className='text-white'>createwealth@thexirr.com.</span>
+</p>
+
+          {/* <p className="text-[13px] text-gray-300">Risk Disclaimer</p>
           <p className="text-[11px] text-gray-500">
             The content in our services, and on this website, is intended to be
             used for educational, research and informational purposes only. We
@@ -276,8 +283,8 @@ export default function Footer() {
             unavailability of our Service, or any special or consequential
             damages that result from the use of, or the inability to use, any or
             all of the content included on our Website or in our Services.
-          </p>
-        </div>
+          </p>*/}
+        </div> 
       </div>
     </footer>
   )
