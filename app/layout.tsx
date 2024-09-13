9a50dd40271e12ee550c35d73a097af88fb62775
NGM <nikhil@rulezero.com>
2024-09-13 18:35:08 +0530

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from '@vercel/analytics/react'
 import '@/app/globals.css'
 import { cn } from '@/lib/utils'
 import { TailwindIndicator } from '@/components/tailwind-indicator'
@@ -8,6 +8,7 @@ import { Providers } from '@/components/providers'
 import { Header } from '@/components/header'
 import { Toaster } from '@/components/ui/sonner'
 import { GoogleTagManager } from '@next/third-parties/google'
+import { headers } from 'next/headers'
 
 export const metadata = {
   metadataBase: process.env.VERCEL_URL
@@ -17,13 +18,17 @@ export const metadata = {
     default: 'Compare Alternative Investments on TheXIRR Platform',
     template: `XIRR INVESTMENT AI`
   },
-  description: 'Looking for alternative investments? TheXIRR simplifies the process. Compare top-performing alternatives, understand their risks, and make informed decisions.',
+  description:
+    'Looking for alternative investments? TheXIRR simplifies the process. Compare top-performing alternatives, understand their risks, and make informed decisions.',
   icons: {
     icon: '/favicon.ico',
     shortcut: '/favicon-16x16.png',
     apple: '/apple-touch-icon.png'
   }
+
 }
+
+
 
 export const viewport = {
   themeColor: [
@@ -37,11 +42,16 @@ interface RootLayoutProps {
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
-
+  const headersList = headers()
+  const host = headersList.get('host') || 'example.com'
+  const protocol = process.env.NODE_ENV === 'production' ? 'https' : 'http'
 
   return (
     <html lang="en" suppressHydrationWarning>
       <GoogleTagManager gtmId="GTM-PSNHGK4D" />
+      <head>
+      <CanonicalTag />
+      </head>
       <body
         className={cn(
           'font-sans antialiased',
@@ -59,7 +69,6 @@ export default function RootLayout({ children }: RootLayoutProps) {
           <div className="flex flex-col min-h-screen bg-gray-900">
             <Header />
             <main className="flex flex-col flex-1 bg-muted/50">{children}</main>
-            
           </div>
           <TailwindIndicator />
         </Providers>
@@ -67,4 +76,14 @@ export default function RootLayout({ children }: RootLayoutProps) {
       </body>
     </html>
   )
+}
+
+
+function CanonicalTag() {
+  const headersList = headers();
+  const host = headersList.get('host') || 'example.com'
+  const path = headersList.get('x-invoke-path') || ''
+  const protocol =  'https'
+  const canonicalUrl = `${protocol}://${host}${path}`
+  return <link rel="canonical" href={canonicalUrl} />
 }
